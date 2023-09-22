9e988ecccefb362d37d8430dd7a0c454f8e22dfa
betouzky <127360597+betouzky@users.noreply.github.com>
2023-09-22 17:17:37 -0600

    22/09/2023 v2

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,32 +1,45 @@
-const express=require('express');
-const pokemon =express.Router();
-const db=require('../config/database');
+const express = require('express');
+const pokemon = express.Router();
+const db = require('../config/database');
 
-pokemon.post("/",(req,res,netx)=>{
-    return res.status(200).send(req.body);
-})
+pokemon.post("/", async (req, res, netx) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
 
-pokemon.get('/', async(req,res,next)=>{
-    const pkmn=await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({code: 1, message:pkmn});
-});
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight,pok_base_experience)";
+        query += `VALUES ('${pok_name}',${pok_height},${pok_weight},${pok_base_experience})`;
 
-pokemon.get('/:id([0-9]{1,3})',async(req,res,next)=>{
-    const id=req.params.id-1;
-    if(id>=1&&id<=722){
-        const pkmn =await db.query("SELECT * FROM pokemon WHERE pok_id="+(id+1)+";");
-        return res.status(200).json({code: 1,message:pkmn});
+        const rows = await db.query(query);
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({ code: 201, message: "Pokemon instertado correctamente" });
+        }
+
+        return res.status(500).json({ code: 500, message: "Ocurrio un error" });
     }
-    return res.status(404).res.send({code: 404, message: "Pokemon no encontrado"});
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
 
-pokemon.get('/:name([A-Za-z]+)',async(req,res,next)=>{
-    const name= req.params.name;
-    const pkmn=await db.query("SELECT *FROM pokemon WHERE pok_name="+name+";");
-    if (pkmn.length>0){
-        return res.status(200).json({code: 1,message:pkmn});
-    } 
-    return res.status(404).res.send({code: 404, message: "Pokemon no encontrado"});
+pokemon.get('/', async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json({ code: 200, message: pkmn });
 });
 
-module.exports=pokemon;
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id - 1;
+    if (id >= 1 && id <= 722) {
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + (id + 1) + ";");
+        return res.status(200).json({ code: 200, message: pkmn });
+    }
+    return res.status(404).res.send({ code: 404, message: "Pokemon no encontrado" });
+});
+
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT *FROM pokemon WHERE pok_name=" + name + ";");
+    if (pkmn.length > 0) {
+        return res.status(200).json({ code: 200, message: pkmn });
+    }
+    return res.status(404).res.send({ code: 404, message: "Pokemon no encontrado" });
+});
+
+module.exports = pokemon;
