b5f257d985d3330bae351122ac0e361b721f4b80
betouzky <127360597+betouzky@users.noreply.github.com>
2023-09-22 15:39:34 -0600

    22/09/23 v1

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,34 +2,31 @@ const express=require('express');
 const pokemon =express.Router();
 const db=require('../config/database');
 
-
 pokemon.post("/",(req,res,netx)=>{
     return res.status(200).send(req.body);
 })
 
 pokemon.get('/', async(req,res,next)=>{
-    const pkmn=await db.query("SELECT*FROM pokemon");
-    return res.status(200).json(pkmn);
+    const pkmn=await db.query("SELECT * FROM pokemon");
+    return res.status(200).json({code: 1, message:pkmn});
 });
 
-pokemon.get('/:id([0-9]{1,3})',(req,res,next)=>{
+pokemon.get('/:id([0-9]{1,3})',async(req,res,next)=>{
     const id=req.params.id-1;
-    if(id>=0&&id<=150){
-        return res.status(200).send(pk[req.params.id-1]);
+    if(id>=1&&id<=722){
+        const pkmn =await db.query("SELECT * FROM pokemon WHERE pok_id="+(id+1)+";");
+        return res.status(200).json({code: 1,message:pkmn});
     }
-    return res.status(404).res.send("Pokemon no encontrado");
+    return res.status(404).res.send({code: 404, message: "Pokemon no encontrado"});
 });
 
-pokemon.get('/:name([A-Za-z]+)',(req,res,next)=>{
+pokemon.get('/:name([A-Za-z]+)',async(req,res,next)=>{
     const name= req.params.name;
-    const pkmn=pk.filter((p)=>{
-        return (p.name.toUpperCase()==name.toUpperCase())&& p;
-    });
-
+    const pkmn=await db.query("SELECT *FROM pokemon WHERE pok_name="+name+";");
     if (pkmn.length>0){
-        return res.status(200).send(pkmn)
+        return res.status(200).json({code: 1,message:pkmn});
     } 
-    return res.status(404).send("Pokemon no encontrado");
+    return res.status(404).res.send({code: 404, message: "Pokemon no encontrado"});
 });
 
 module.exports=pokemon;
