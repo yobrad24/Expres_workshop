8475b84369527f9d28fca21732501aefa2467a41
betouzky <127360597+betouzky@users.noreply.github.com>
2023-10-06 11:54:24 -0600

    06/10/23 v2

--- index.js
+++ index.js
@@ -3,25 +3,21 @@ const express= require('express');
 const app=express();
 const pokemon=require('./routes/pokemon');
 const user=require('./routes/user');
+const auth =require('./middleware/auth');
+const notFound=require('./middleware/notFound');
+const index=require ('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get("/",(req,res,next)=>{
-   return res.status(200).json({code: 1,message: "Bienvenido al Pokedex"})
-});
-
-app.use("/pokemon",pokemon);
-
+app.get("/",index);
 app.use("/usuario",user);
+app.use(auth);
+app.use("/pokemon",pokemon);
+app.use(notFound);
 
-app.use((req,res,next)=>{
-    return res.status(404).json({code: 404,message: "URL no encontrada"})
-});
-
-app.listen(process.env.PORT||3000,()=>
-    {
+app.listen(process.env.PORT||3000,()=>{
         console.log("Server is running... ");
     }
 );
