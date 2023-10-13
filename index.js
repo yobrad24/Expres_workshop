0d04abae875dca2fee9186a2f0e78d88c4c8b2c0
betouzky <127360597+betouzky@users.noreply.github.com>
2023-10-13 13:07:18 -0600

    13/10/23 v1

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const morgan=require('morgan');
 const express= require('express');
 const app=express();
+
 const pokemon=require('./routes/pokemon');
 const user=require('./routes/user');
+
 const auth =require('./middleware/auth');
 const notFound=require('./middleware/notFound');
 const index=require ('./middleware/index');
+const cors=require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -19,5 +23,4 @@ app.use(notFound);
 
 app.listen(process.env.PORT||3000,()=>{
         console.log("Server is running... ");
-    }
-);
+    });
